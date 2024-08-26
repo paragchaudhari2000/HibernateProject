89d420d81cefdd19a4153f100361a7dd352dbf77
rajep <rajep@MSI>
2024-08-26 23:31:45 +0530

    First Commit

---
+++ Hibernate02/src/main/java/com/Model/Employee.java
@@ -0,0 +1,33 @@
+package com.Model;
+
+public class Employee {
+
+	int eid;
+	String ename;
+	double esalary;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public double getEsalary() {
+		return esalary;
+	}
+	public void setEsalary(double esalary) {
+		this.esalary = esalary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", esalary=" + esalary + "]";
+	}
+	
+	
+	
+}
